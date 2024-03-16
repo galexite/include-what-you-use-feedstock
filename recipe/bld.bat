c1c0c3ebd98cfa580f06cd41a59a2780e66f624d
George White <me@galexite.uk>
2024-03-16 21:56:50 +0000

    Write out Python script then execute

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,15 +1,13 @@
 REM TODO(galexite): remove once llvm/llvm-project#83807 has been merged
-python -c ^" ^
-from pathlib import Path; ^
-import re; ^
-^
-cmakelists = Path.cwd() / 'CMakeLists.txt'; ^
-contents = cmakelists.read_text(encoding='utf-8'); ^
-contents = re.sub(r'(cmake_minimum_required\([^^\n]+\))', r'\1\ninclude(GNUInstallDirs)', contents, count=1); ^
-cmakelists.write_text(contents, encoding='utf-8') ^
-^"
-
+echo from pathlib import Path                                                                                     >> _windows_patch.py
+echo import re                                                                                                    >> _windows_patch.py
+echo cmakelists = Path.cwd() / 'CMakeLists.txt'                                                                   >> _windows_patch.py
+echo contents = cmakelists.read_text(encoding='utf-8')                                                            >> _windows_patch.py
+echo contents = re.sub(r'(cmake_minimum_required\([^^\n]+\))', r'\1\ninclude(GNUInstallDirs)', contents, count=1) >> _windows_patch.py
+echo cmakelists.write_text(contents, encoding='utf-8')                                                            >> _windows_patch.py
+python _windows_patch.py
 if %ERRORLEVEL% neq 0 exit 1
+del _windows_patch.py
 
 mkdir build
 cd build
